05944bed03923e0b490cef00cbef324b8f2037d3
Ivan Paulino <softip@gmail.com>
2021-01-07 16:24:02 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -25,6 +25,7 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String address;
 	private Double latitude;
 	private Double longitude;
@@ -44,7 +45,6 @@ public class Order implements Serializable{
 
 
 	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
-		super();
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
